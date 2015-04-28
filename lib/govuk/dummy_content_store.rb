77b0b5978c4d190851cc757f0a636c0cdf466e3c
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-04-28 17:30:39 +0100

    Initial commit

---
+++ lib/govuk/dummy_content_store.rb
@@ -0,0 +1,7 @@
+require "govuk/dummy_content_store/version"
+
+module Govuk
+  module DummyContentStore
+    # Your code goes here...
+  end
+end
---
+++ lib/govuk/dummy_content_store/version.rb
@@ -0,0 +1,5 @@
+module Govuk
+  module DummyContentStore
+    VERSION = "0.0.1"
+  end
+end
